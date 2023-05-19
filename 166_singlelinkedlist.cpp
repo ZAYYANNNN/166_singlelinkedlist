a13746d87cead43d3a275137a6002b0032a2209c
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-05-19 15:55:09 +0700

    menjadikan node previous dan node current start

--- 166_singlelinkedlist.cpp
+++ 166_singlelinkedlist.cpp
@@ -28,4 +28,27 @@ void addNode() {
 
 
 		}
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs) {
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
+
+
 
