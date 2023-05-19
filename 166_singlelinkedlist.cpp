b32c2b11b38886d0d666d7eeba7babbfaf89c556
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-05-19 15:47:37 +0700

    void node dan display nama dan nim

--- 166_singlelinkedlist.cpp
+++ 166_singlelinkedlist.cpp
@@ -1,1 +1,23 @@
-#sdada
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int noMhs;
+	string name;
+	Node* next;
+};
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan Nim : ";
+	cin >> nim;
+	cout << "Masukan Nama : ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	
