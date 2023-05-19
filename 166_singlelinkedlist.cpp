0e9129b66aa61f438d9e4fade148af2ef05cda57
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-05-19 15:56:35 +0700

    looping

--- 166_singlelinkedlist.cpp
+++ 166_singlelinkedlist.cpp
@@ -37,18 +37,4 @@ void addNode() {
 
 	while ((current != NULL) && (nim >= current->noMhs))
 	{
-		if (nim == current->noMhs) {
-			cout << "NIM sudah ada" << endl;
-			return;
-		}
-
-		previous = current;
-		current = current->next;
-	}
-
-	nodeBaru->next = current;
-	previous->next = nodeBaru;
-}
-
-
-
+	
